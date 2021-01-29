36d3e779c72e7ca2b9a00fb36c569464bb2cda9c
AlejandroVillarreal <alexvodark@hotmail.com>
2021-01-28 20:29:01 -0600

    setup
    Minimal application
    Html
    Css

---
+++ server.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+print(__name__)
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+@app.route('/<username>/<int:post_id>')
+def show_username(username=None,post_id=None):
+    return render_template('index.html',name=username,post_id=post_id)
